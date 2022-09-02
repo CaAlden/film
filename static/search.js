ade4dfd1d239a4cf855b7545727d8a7f0421b0f5
Campbell Alden <campbell.alden@bibo.com.ph>
2022-09-02 19:33:04 +0900

    PUBLISH

--- static/search.js
+++ static/search.js
@@ -1,8 +1,12 @@
 const searchInput = document.getElementById('movie-search');
 const datalist = document.getElementById('movies');
 const dataitems = new Map();
+const dataSummaries = new Map();
 Array.from(datalist.children).forEach(function (ch) {
   dataitems.set(ch.value, ch.getAttribute('data-value'));
+  if (navigator.userAgent.match(/firefox/i)) {
+    ch.innerHTML = ch.value;
+  }
 });
 
 searchInput.addEventListener('change', function(e) {
